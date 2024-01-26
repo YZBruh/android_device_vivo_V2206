e939ce3500041031f531af4e95c3ddd845605e89
YZBruh <yagizzengin73@gmail.com>
2024-01-26 23:17:54 +0300

    İnitial update

--- twrp_V2206.mk
+++ twrp_V2206.mk
@@ -7,23 +7,21 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from V2206 device
 $(call inherit-product, device/vivo/V2206/device.mk)
 
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 PRODUCT_DEVICE := V2206
-PRODUCT_NAME := omni_V2206
+PRODUCT_NAME := twrp_V2206
 PRODUCT_BRAND := vivo
 PRODUCT_MODEL := V2206
 PRODUCT_MANUFACTURER := vivo
 
 PRODUCT_GMS_CLIENTID_BASE := android-vivo
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="qssi-user 13 TP1A.220624.014 eng.compil.20230804.011548 release-keys"
-
-BUILD_FINGERPRINT := vivo/V2206/V2206:13/TP1A.220624.014/compiler08040115:user/release-keys
