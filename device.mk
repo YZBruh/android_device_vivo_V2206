ce4b747c25022f9ab2ec65a6cb1b60f22d6ea987
YZBruh <yagizzengin73@gmail.com>
2024-02-17 10:46:17 +0300

    Cleanup

--- device.mk
+++ device.mk
@@ -60,9 +60,9 @@ TARGET_SCREEN_WIDTH := 720
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := V2206
-
 TARGET_COPY_OUT_VENDOR := vendor
 
+# Decryption
 PRODUCT_PACKAGES += \
     qcom_decrypt \
     qcom_decrypt_fbe
@@ -106,7 +106,6 @@ TW_INCLUDE_FASTBOOTD := true
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2 \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so \
-    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so 
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so \
