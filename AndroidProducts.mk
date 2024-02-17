73d2de2c472275fd3dac2ef5faf2432f68966211
YZBruh <yagizzengin73@gmail.com>
2024-02-17 11:16:00 +0300

    Cleanup code

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,5 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_V2206.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_V2206-user \
-    twrp_V2206-userdebug \
     twrp_V2206-eng
